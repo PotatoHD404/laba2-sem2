ef61487441a2cabd56b116f2288866f57544e38b
PotatoHD1 <31506993+PotatoHD1@users.noreply.github.com>
2021-04-10 21:33:58 +0300

    added almost everything

--- ui/src/app.py
+++ ui/src/app.py
@@ -1,30 +1,81 @@
 from flask import Flask, render_template, url_for, request, redirect, make_response, Response, session
 from flask_socketio import SocketIO, emit
+from celery import Celery
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_session import Session
+from flask_session import Session, SqlAlchemySessionInterface
 from datetime import datetime
 from secrets import token_urlsafe
+import threading
+from time import sleep
 import paramiko
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'LbcDtbci8PzffxMQHvgvgdWxjBPrzzoLxuBN4PzK014'
+app.secret_key = 'LbcDtbci8PzffxMQHvgvgdWxjBPrzzoLxuBN4PzK014'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-app.config['SESSION_TYPE'] = 'sqlalchemy'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True  # not quiet warning message
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 sess = Session(app)
-
-app.config['SESSION_SQLALCHEMY_TABLE'] = 'sessions'
-app.config['SESSION_SQLALCHEMY'] = db
-sess.app.session_interface.db.create_all()
+SqlAlchemySessionInterface(app, db, "sessions", "sess_")
+sess.init_app(app)
+# sess.app.session_interface.db.create_all()
 
 socketio = SocketIO(app, manage_session=False)
 
+app.config['CELERY_BROKER_URL'] = 'redis://redis-server:6379/0'
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
+
 clientsList = {}
+
+
+@celery.task(bind=True)
+def thread_function():
+    while True:
+        for token, j in zip(clientsList.keys(), clientsList.values()):
+            emit('log', 'working', room=j[1])
+            # channel = j[0]
+            # res = ""
+            # tmp = b""
+            #
+            # if channel.recv_ready():
+            #     tmp = channel.recv(4096)
+            # while len(tmp) == 4096:
+            #     res += tmp.decode('utf-8')
+            #     tmp = channel.recv(4096)
+            # res += tmp.decode('utf-8')
+            # exists = len(ConsoleText.query.filter_by(token=token).all()) > 0
+            # if res != "":
+            #     if exists:
+            #         ConsoleText.query.filter_by(token=token).first().content += res
+            #     else:
+            #         new_text = ConsoleText(content=res, token=token)
+            #
+            #     try:
+            #         if not exists:
+            #             db.session.add(new_text)
+            #         db.session.commit()
+            #         if not exists:
+            #             exists += 1
+            #     except Exception:
+            #         'Pass'
+            #     emit('refresh', {'token': token,
+            #                      'text': ConsoleText.query.filter_by(token=token).first().content if exists else ""},
+            #          room=j[1])
+        sleep(0.5)
+
+
+if __name__ == '__main__':
+    x = threading.Thread(target=thread_function)
+    x.start()
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect("core", username="user", password="password")
 
 
 class ConsoleText(db.Model):
@@ -37,62 +88,10 @@ class ConsoleText(db.Model):
         return '<Row %r>' % self.id
 
 
-@app.route('/api/get_text', methods=['GET'])
-def resp(channel):
-    if not request.cookies.get('token'):
-        res = make_response(redirect("/api/get_text"))
-        res.set_cookie('token', token_urlsafe(16), max_age=60 * 60 * 24 * 30)
-        return res
-
-    res = ""
-    tmp = b""
-
-    if channel.recv_ready():
-        tmp = channel.recv(4096)
-    while len(tmp) == 4096:
-        res += tmp.decode('utf-8')
-        tmp = channel.recv(4096)
-    res += tmp.decode('utf-8')
-    token = request.cookies.get('token')
-    exists = len(ConsoleText.query.filter_by(token=token).all()) > 0
-    if res != "":
-        if exists != 0:
-            ConsoleText.query.filter_by(token=token).first().content += res
-        else:
-            new_text = ConsoleText(content=res, token=token)
-
-        try:
-            if not exists:
-                db.session.add(new_text)
-            db.session.commit()
-            if not exists:
-                exists += 1
-        except Exception:
-            'Pass'
-
-    if exists or res != "":
-        res = ConsoleText.query.filter_by(token=token).first().content
-
-    return res
-
-
 def login():
     if not session.get('token'):
         token = token_urlsafe(16)
         session['token'] = token
-
-
-def send(cmd, channel):
-    channel.sendall(f'{cmd}\n')
-    res = ""
-    tmp = b""
-    if channel.recv_ready():
-        tmp = channel.recv(4096)
-    while len(tmp) == 4096:
-        res += tmp.decode('utf-8')
-        tmp = channel.recv(4096)
-    res += tmp.decode('utf-8')
-    return res
 
 
 @app.route('/', methods=['GET'])
@@ -107,43 +106,63 @@ def index():
 @socketio.on('connect')
 def connect():
     login()
-    # if __name__ == '__main__':
-    #     client = paramiko.SSHClient()
-    # client.load_system_host_keys()
-    # client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    # client.connect("core", username="user", password="password")
-    # # ssh_stdin, ssh_stdout, ssh_stderr = client.exec_command("cd /app && ./Tests")
-    # # client.exec_command("cd /app", get_pty=True)
-    # # ssh_stdin, ssh_stdout, ssh_stderr = client.exec_command("cd /app && ./Tests")
-    # channel = client.get_transport().open_session()
-    # # tmp = channel.get_pty()
-    # channel.invoke_shell()
-    exists = len(ConsoleText.query.filter_by(token=session['token']).all()) > 0
+    global client
+    global clientsList
     token = session['token']
+    exists = len(ConsoleText.query.filter_by(token=token).all()) > 0
 
+    if token not in clientsList:
+        # ssh_stdin, ssh_stdout, ssh_stderr = client.exec_command("cd /app && ./Tests")
+        # client.exec_command("cd /app", get_pty=True)
+        # ssh_stdin, ssh_stdout, ssh_stderr = client.exec_command("cd /app && ./Tests")
+        channel = client.get_transport().open_session()
+        # tmp = channel.get_pty()
+        channel.invoke_shell()
+        clientsList[token] = [channel, request.sid]
+
+    emit('log', str(clientsList))
     emit('refresh', {'token': token,
                      'text': ConsoleText.query.filter_by(token=token).first().content if exists else ""})
 
 
 @socketio.on('disconnect')
 def disconnect():
+    global clientsList
+    token = session['token']
+    if token in clientsList:
+        del clientsList[token]
     print('Client disconnected')
 
 
-@socketio.on('restart')
-def restart(json):
-    print('received json: ' + str(json))
+# @socketio.on('restart')
+# def restart(json):
+#     print('received json: ' + str(json))
+
+
+@socketio.on('clear')
+def clear():
+    token = session['token']
+    exists = len(ConsoleText.query.filter_by(token=token).all()) > 0
+    if exists:
+        ConsoleText.query.filter_by(token=token).first().content = ""
+        db.session.commit()
+    emit('refresh', {'token': token,
+                     'text': ConsoleText.query.filter_by(token=token).first().content if exists else ""})
 
 
 @socketio.on('command')
-def command(json):
-    emit('response', 'Connected')
+def command():
+    token = session['token']
+    emit('log', str(clientsList))
+    # emit('response', 'Connected')
 
 
 @socketio.on('tests')
-def get():
-    send("cd /app")
-    send("./Tests --gtest_repeat=1 --gtest_color=no")
+def tests():
+    channel = clientsList[session['token']][0]
+    channel.sendall(f'cd /app\n')
+    channel.sendall(f'./Tests --gtest_repeat=1 --gtest_color=no\n')
+    emit('log', 'tests started')
 
 
 if __name__ == '__main__':
--- ui/src/static/js/index.js
+++ ui/src/static/js/index.js
@@ -2,11 +2,21 @@ let socket = io();
 socket.on('connect', function() {
 });
 socket.on('refresh', function(data) {
-    // console.log(data.token, data.text)
+    console.log('refreshed')
     document.getElementById('token').innerText = data.token
     document.getElementById('consoleOutput').innerText = data.text
 });
+socket.on('log', function(data) {
+    console.log(data)
+});
+function clear_console() {
+    socket.emit('clear');
+}
 
-function tests() {
+function run_tests() {
     socket.emit('tests');
+}
+
+function command() {
+    socket.emit('command');
 }
