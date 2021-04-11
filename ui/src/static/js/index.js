a5cfc2b5f33708da2493e0180b3d284a19846110
PotatoHD1 <31506993+PotatoHD1@users.noreply.github.com>
2021-04-11 17:03:13 +0300

    perfect js

--- ui/src/static/js/index.js
+++ ui/src/static/js/index.js
@@ -1,5 +1,8 @@
 let socket = io();
+
 socket.on('connect', function() {
+    let textarea1 = document.getElementById('consoleOutput');
+    textarea1.scrollTop = textarea1.scrollHeight;
 });
 
 socket.on('refresh', function(data) {
@@ -7,7 +10,18 @@ socket.on('refresh', function(data) {
     document.getElementById('token').innerText = data.token
     document.getElementById('consoleOutput').innerHTML = data.text
     let textarea = document.getElementById('consoleOutput');
-    textarea.scrollTop = textarea.scrollHeight;
+    let temp = textarea.scrollTop;
+    let interval = setInterval(()=>{
+        let end = textarea.scrollHeight;
+        if(temp < end){
+            textarea.scrollTop += 50;
+            temp += 50;
+        }
+        else{
+            clearInterval(interval);
+        }
+        console.log(temp)
+    }, 5)
 
 });
 socket.on('log', function(data) {
