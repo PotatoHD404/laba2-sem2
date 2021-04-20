e37f6153ba73642b636114a0977a8c2c69768e7c
PotatoHD404 <31506993+PotatoHD404@users.noreply.github.com>
2021-04-21 01:06:04 +0300

    Fixed tests

--- core/src/Tests/PolynomialTests.cpp
+++ core/src/Tests/PolynomialTests.cpp
@@ -9,23 +9,23 @@ using namespace std;
 
 TEST(Polynomial, Sum) {
     Polynomial<int> poly1 = {1, 2, 1};
-    Polynomial<int> poly2 = {1, 2, 3, 4, 5};
+    Polynomial<int> poly2 = {5, 4, 3, 2, 1};
     poly2 = poly1 + poly2;
-    ASSERT_EQ(poly2.GetDimension(),5);
+    ASSERT_EQ(poly2.GetDimension(), 5);
     EXPECT_EQ(poly2[0], 5);
     EXPECT_EQ(poly2[1], 4);
     EXPECT_EQ(poly2[2], 4);
     EXPECT_EQ(poly2[3], 4);
     EXPECT_EQ(poly2[4], 2);
     poly2 = Polynomial(poly1);
-    ASSERT_EQ(poly2.GetDimension(),3);
+    ASSERT_EQ(poly2.GetDimension(), 3);
     EXPECT_EQ(poly2[0], 1);
     EXPECT_EQ(poly2[1], 2);
     EXPECT_EQ(poly2[2], 1);
 }
 
 TEST(Polynomial, Multiply) {
-    Polynomial<int> poly1 = {1, 2, 5};
+    Polynomial<int> poly1 = {5, 2 ,1};
     Polynomial<int> poly2 = poly1 * 2;
     EXPECT_EQ(poly2[0], 10);
     EXPECT_EQ(poly2[1], 4);
